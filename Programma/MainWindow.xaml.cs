863ef09efe5a5673c65e107d814cc941f2831e94
Dirty Joy <gethardrekt@mail.ru>
2024-10-11 22:10:49 +0300

    Initial Commit

--- Programma/MainWindow.xaml.cs
+++ Programma/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OfficeOpenXml;
+using Programma.Resources;
 
 namespace Programma
 {
@@ -19,6 +21,35 @@ namespace Programma
         public MainWindow()
         {
             InitializeComponent();
+            Table.ItemsSource = ReadExcelFile("Resources/data.xlsx");
+        }
+        public List<DataModel> ReadExcelFile(string filePath)
+        {
+            List<DataModel> data = new List<DataModel>();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(filePath))
+            {
+                var worksheet = package.Workbook.Worksheets.First();
+                for (int row = 2; row <= worksheet.Dimension.End.Row; row++)
+                {
+                    try
+                    {
+                        var rowData = new DataModel
+                        {
+                            Id = Int16.Parse(worksheet.Cells[row, 1].Value.ToString()),
+                            FIO = worksheet.Cells[row, 2].Value.ToString(),
+                            Otdel = worksheet.Cells[row, 3].Value.ToString(),
+                            Setup = DateTime.Parse(worksheet.Cells[row, 4].Value.ToString()),
+                            Start = DateTime.Parse(worksheet.Cells[row, 5].Value.ToString()),
+                            End = DateTime.Parse(worksheet.Cells[row, 6].Value.ToString()),
+                            Status = worksheet.Cells[row, 7].Value.ToString(),
+                        };
+                        data.Add(rowData);
+                    }
+                    catch (Exception) {}
+                }
+            }
+            return data;
         }
     }
 }
---
+++ Programma/Resources/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programma.Resources
+{
+    public class DataModel
+    {
+        public short Id { get; set; }
+        public string? FIO { get; set; }
+        public string? Otdel { get; set; }
+        public DateTime? Setup {  get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        //public string ExpireStatus { get; set; }
+        public string? Status { get; set; } //Подан запрос, получен, активен 
+    }
+}
